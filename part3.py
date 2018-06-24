dcb0e7ddc2d6ee01f9b4a1f1966ac2e357cded18
Vishal Reddy <reddykvishal@gmail.com>
2018-06-24 16:03:01 -0400

    Add files via upload

--- part3.py
+++ part3.py
@@ -6,3 +6,20 @@ from bs4 import BeautifulSoup
 # write your code here
 # usage should be python3 part3.py
 
+url = "https://www.michigandaily.com/"
+soup = BeautifulSoup(requests.get(url).content, 'html.parser')
+
+mostRead = soup.find("div", attrs={'class': 'view-most-read'}).find("ol").find_all("li")
+
+print("Michigan Daily -- MOST READ")
+
+for listItem in mostRead:
+	print(listItem.find("a").text.strip())
+
+	try:
+		innerSoup = BeautifulSoup(requests.get(url + listItem.find("a")['href']).content, 'html.parser')
+		author= innerSoup.find("div", attrs = {"class":"byline"}).find('div').find('a').text.strip()
+		print("by " + author)
+
+	except:
+		print("No byline author found")
